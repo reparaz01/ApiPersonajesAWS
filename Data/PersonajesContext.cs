e43942de652123b1dcaf51137a1fa9b2c0ebf011
reparaz01 <jreparazm@gmail.com>
2024-05-16 11:26:46 +0200

    Add project files.

---
+++ Data/PersonajesContext.cs
@@ -0,0 +1,18 @@
+﻿using ApiPersonajesAWS.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiPersonajesAWS.Data
+{
+    public class PersonajesContext : DbContext
+
+    {
+
+        public PersonajesContext(DbContextOptions<PersonajesContext>
+
+            options) : base(options) { }
+
+        public DbSet<Personaje> Personajes { get; set; }
+
+    }
+}
